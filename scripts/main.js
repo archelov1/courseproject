5d394e37f24411d11e5537b93607ba704d9cb7ec
archelov1 <wevchuk13@gmail.com>
2024-11-17 17:20:21 +0200

    add workshop section

--- scripts/main.js
+++ scripts/main.js
@@ -26,4 +26,27 @@ function courses_swiper() {
   });
 }
 
+function workshop_swiper() {
+  const swiper = new Swiper(".workshop__swiper", {
+    slidesPerView: 1,
+    spaceBetween: 48,
+
+    pagination: {
+      el: ".workshop__swiper-pagination",
+      type: "bullets",
+    },
+
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+      },
+
+      768: {
+        slidesPerView: 2,
+      },
+    },
+  });
+}
+
 courses_swiper();
+workshop_swiper();
